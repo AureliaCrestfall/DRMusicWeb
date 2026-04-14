5a5dcb7c4bda905576df7b658033542f14c85195
Carina43 <cag003@edu.zealand.dk>
2026-04-14 11:36:58 +0200

    Get All metode

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-fef
+const baseUrl = "http://localhost:5137/API/Music"
+
+Vue.createApp({
+    data() {
+        return {
+            records: [],
+            singleRecord: null,
+            deleteId: null,
+            deleteMessage: "",
+            addData: { title: "", artist: "", duration: null, publicationYear: "" },
+            addMessage: "",
+            updateData: { id: null, title: "", artist: "", duration: null, publicationYear: "" },
+            updateMessage: ""
+        }
+    },
+    methods: {
+        getAllRecords() {
+            this.getRecords(baseUrl)
+    },
+
+    async getRecords(url) {
+    try {
+        const response = await axios.get(url)
+        this.records = await response.data
+    } catch (ex) {
+        alert(ex.message)
+    }
+    },
+     async getById(id) {
+            if (id === null || id === undefined || isNaN(id) || id <= 0) {
+                alert("Please enter a valid record ID")
+                return
+            }
+            const url = baseUrl + "/" + id
+            try {
+                const response = await axios.get(url)
+                this.singleRecord = await response.data
+            } catch (ex) {
+                this.singleRecord = null
+                alert(ex.message)
+            }
+        },
+    
+     
+     
+}
+}).mount("#app")
