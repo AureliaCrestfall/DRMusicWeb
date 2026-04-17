cebb7ef65bdadd7ee95f11b67b9c6413f74842aa
Carina43 <cag003@edu.zealand.dk>
2026-04-17 10:36:37 +0200

    login  etc

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+authUrl = "http://localhost:5137/API/Auth/Login/";
+
 const baseUrl = "http://localhost:5137/API/Music"
+
 
 Vue.createApp({
     data() {
         return {
             records: [],
-            allrecords:[],
+            allrecords: [],
             singleRecord: null,
             deleteId: null,
             deleteMessage: "",
@@ -12,45 +15,57 @@ Vue.createApp({
             addMessage: "",
             updateData: { id: null, title: "", artist: "", duration: null, publicationYear: "" },
             updateMessage: "",
-            artist: null
+            artist: null,
+            auth: { username: "", password: "" },
+            loggedIn: false,
+            authMessage: null,
+            jwtToken: null,
+            role: null
         }
     },
     methods: {
+        login() {
+            axios.post(authUrl, this.auth)
+                .then(response => {
+                    this.jwtToken = response.data.token;
+                    this.role = response.data.role;
+                    this.loggedIn = true;
+                    this.authMessage = "Authentication successful";
+                    this.getRecords(); // Fetch records immediately after successful login
+                }).catch(ex => {
+                    this.authMessage = "Authentication failed - " + ex.message;
+                });
+        },
+        logout() {
+            this.jwtToken = null;
+            this.role = null;
+            this.loggedIn = false;
+            this.auth = { username: "", password: "" };
+            this.records = [];
+            this.message = null;
+            this.authMessage = "Logged out successfully";
+        },
+
         getAllRecords() {
             this.getRecords(baseUrl)
-    },
+        },
 
-    async getRecords(url) {
-    try {
-        const response = await axios.get(url)
-        this.allrecords = await response.data
-
-    } catch (ex) {
-        alert(ex.message)
-    }
-    },
-     async getById(id) {
-            if (id === null || id === undefined || isNaN(id) || id <= 0) {
-                alert("Please enter a valid record ID")
-                return
-            }
-            const url = baseUrl + "/" + id
+        async getRecords(url) {
             try {
                 const response = await axios.get(url)
-                this.singleRecord = await response.data
+                this.records = await response.data
+
             } catch (ex) {
-                this.singleRecord = null
                 alert(ex.message)
             }
         },
-        filterByTitle(artist) {
-            console.log("Title:" + artist + ":")
-            console.log("All books " + this.allrecords)
-            this.records = this.allrecords.filter(b => b.artist.includes(artist))
-            console.log("filtered Books: " + this.records)
+        async filterByTitle(title) {
+            await this.getRecords(baseUrl)
+            console.log("Title:" + title + ":")
+            console.log("All records " + this.records)
+            this.allrecords = this.records.filter(b => b.title.includes(title))
+            console.log("filtered records: " + this.records)
         }
-    
-     
-     
-}
+
+    }
 }).mount("#app")
