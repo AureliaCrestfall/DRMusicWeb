d38b581c3da368b8966e46977e38e567c268db81
Carina43 <cag003@edu.zealand.dk>
2026-04-17 12:17:10 +0200

    delete og put/update done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ Vue.createApp({
         }
     },
     methods: {
-       async login() {
+        async login() {
             axios.post(authUrl, this.auth)
                 .then(response => {
                     this.jwtToken = response.data.token;
@@ -46,7 +46,7 @@ Vue.createApp({
             this.authMessage = "Logged out successfully";
         },
 
-       async  getAllRecords() {
+        async getAllRecords() {
             this.getRecords(baseUrl)
         },
 
@@ -66,7 +66,7 @@ Vue.createApp({
             this.allrecords = this.records.filter(b => b.title.includes(title))
             console.log("filtered records: " + this.records)
         },
-         addRecord() {
+        addRecord() {
             axios.post(baseUrl, this.addData, {
                 headers: { // TODO - move to interceptor
                     'Authorization': 'Bearer ' + this.jwtToken
@@ -80,34 +80,47 @@ Vue.createApp({
                 .catch(ex => {
                     this.message = "Failed to add record - " + ex.message;
                 });
-        }, deleteRecord() {
-            axios.delete(baseUrl, this.deleteId, {
-                headers: { // TODO - move to interceptor
-                    'Authorization': 'Bearer ' + this.jwtToken
-                }
-            })
-                .then(response => {
-                    this.records.push(response.data);
-                    this.message = "record added successfully";
-                    this.deleteId = null; // Reset the form
-                })
-                .catch(ex => {
-                    this.message = "Failed to add record - " + ex.message;
+        },
+        async deleteRecord() {
+            try {
+                await axios.delete(`${baseUrl}/${this.deleteId}`, {
+                    headers: {
+                        Authorization: 'Bearer ' + this.jwtToken
+                    }
                 });
-        }, updateDataRecord() {
-            axios.put(baseUrl, this.updateData, {
-                headers: { // TODO - move to interceptor
-                    'Authorization': 'Bearer ' + this.jwtToken
+
+                this.records = this.records.filter(r => r.id !== this.deleteId);
+                this.deleteId = null;
+                this.message = "Record deleted successfully";
+
+            } catch (ex) {
+                this.message = "Failed to delete record - " + ex.message;
+            }
+        },
+        async updateDataRecord() {
+            try {
+                const response = await axios.put(
+                    `${baseUrl}/${this.updateData.id}`,
+                    this.updateData,
+                    {
+                        headers: { Authorization: 'Bearer ' + this.jwtToken }
+                    }
+                );
+
+                const index = this.records.findIndex(r => r.id === this.updateData.id);
+                if (index !== -1) {
+                    this.records[index] = response.data;
                 }
-            })
-                .then(response => {
-                    this.records.push(response.data);
-                    this.message = "record added successfully";
-                    this.updateData = { id: null, title: "", artist: "", duration: null, publicationYear: "" }; // Reset the form
-                })
-                .catch(ex => {
-                    this.message = "Failed to add record - " + ex.message;
-                });
+
+                this.updateData = {
+                    id: null, title: "", artist: "", duration: null, publicationYear: ""
+                };
+
+                this.message = "Record updated successfully";
+
+            } catch (ex) {
+                this.message = "Failed to update record - " + ex.message;
+            }
         }
 
     }
